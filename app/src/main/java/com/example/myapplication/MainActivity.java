2d7a879ea220140d1eab5cb9617d84b0508925c1
OskariSuonpaa <oskari.suonpaa@student.lut.fi>
2021-03-08 22:27:47 +0200

    7.2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
     }
     public void painike(View v){
         System.out.println("Hello World!");
+        text.setText("Hello World!");
     }
 }
